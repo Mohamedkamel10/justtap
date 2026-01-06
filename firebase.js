34c35708fe9774ff416ffa285c4b86751a28e46e
Mohamedkamel10 <111062223+Mohamedkamel10@users.noreply.github.com>
2026-01-06 23:45:08 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ firebase.initializeApp({
   apiKey: "AIzaSyBgIH7EBZy-FFipEtBf0u1Db5uH6tVGKW8",
   authDomain: "just-tap-4e85e.firebaseapp.com",
   projectId: "just-tap-4e85e",
-  storageBucket: "just-tap-4e85e.firebasestorage.app",
+  storageBucket: "just-tap-4e85e.appspot.com",
   messagingSenderId: "497081794470",
   appId: "1:497081794470:web:f14285e82562c2292d5967"
 });
